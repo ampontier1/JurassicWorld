50d575f382be295b1f6168cdfba8b04696492c9f
ampontier1 <78193512+ampontier1@users.noreply.github.com>
2021-10-18 00:17:03 -0500

    changes to help

--- src/main/java/controller/ViewAllSurveysServlet.java
+++ src/main/java/controller/ViewAllSurveysServlet.java
@@ -30,8 +30,6 @@ public class ViewAllSurveysServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
 		
 		ParkSurveyHelper psh = new ParkSurveyHelper();
 		List<ParkSurvey> abc = psh.getSurvey();
